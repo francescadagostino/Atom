0402c83b9c525c6ecf8193c8c35da96c62f78039
francescadagostino <frda4410@colorado.edu>
2021-12-10 14:11:40 -0700

    friday

--- Week Final/index.js
+++ Week Final/index.js
@@ -1,6 +1,3 @@
-
-function setActive(e){
-
 document.getElementById("submit").addEventListener("click", setActiveButton);
 function setActiveButton(e){
   document.getElementsByTagName('input')[0].value ="";
